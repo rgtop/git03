9d7d30d4f105de55e8517adac54abf6562c76ff2
rgtop <rgtop@qq.com>
2021-01-30 09:28:33 +0800

    主干操作

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -19,5 +19,6 @@ public class AppTest
         System.out.println("测试2");
         System.out.println("远程添加分支，test03");
         System.out.println("测试过滤提交");
+        System.out.println("主干操作");
     }
 }
